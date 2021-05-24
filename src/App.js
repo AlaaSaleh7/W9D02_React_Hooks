a364f9cb399186a67ba04f0afd5494c00f942c37
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-24 12:41:44 +0300

    1+2+3

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+
+
+const [posts, setPosts]=useState()
+// jsx
+const App = () => {
+  return <>
+  <div><h1> Blog App </h1> </div>
+  </>;
+};
+
+export default App
